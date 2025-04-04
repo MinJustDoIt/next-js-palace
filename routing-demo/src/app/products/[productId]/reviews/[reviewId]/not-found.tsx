a7e8d1148a4fafd6be0c02fcf5f8945fa2bb7211
minthihalwin <minthihalwin.dev@gmail.com>
2025-04-05 00:04:45 +1300

    enhance NotFound component to display specific review and product IDs in error message

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,9 +1,16 @@
+"use client";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const NotFound = () => {
+  const pathName = usePathname();
+  const productId = pathName.split("/")[2];
+  const reviewId = pathName.split("/")[4];
   return (
     <div>
-      <h2>Review not Found</h2>
+      <h2>
+        Review {reviewId} not found for product {productId}
+      </h2>
     </div>
   );
 };
