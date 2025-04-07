21bed327ccee11ebf84cd8923e0c11073b05e937
minthihalwin <minthihalwin.dev@gmail.com>
2025-04-07 14:21:11 +1200

    add PATCH and DELETE methods to comments API; enhance GET method with query filtering

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -5,8 +5,30 @@ export async function GET(
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-  console.log("id", id);
   const comment = comments.find((comment) => comment.id === parseInt(id));
-  console.log("comment", comment);
   return Response.json(comment);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComment);
 }
